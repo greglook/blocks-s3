ece04251841014399622db5b2629628788c1ccf5
Greg Look <greg@greg-look.net>
2015-11-11 23:15:48 -0800

    Add basic integration tests for s3-store.

--- test/blocks/store/s3_test.clj
+++ test/blocks/store/s3_test.clj
@@ -1,11 +1,21 @@
 (ns blocks.store.s3-test
   (:require
-    [clojure.test :refer :all]
-    [blocks.core :as block]
-    [blocks.store.s3 :refer [s3-store]]))
+    (blocks.store
+      [s3 :refer [s3-store]]
+      [tests :refer [test-block-store]])
+    [clojure.test :refer :all]))
 
 
-#_
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; TODO: unit tests
+
+
+(deftest ^:integration test-s3-store
+  (let [store (s3-store "greglook-data"
+                        :prefix (str "s3-block-store."
+                                     (System/currentTimeMillis))
+                        :region :us-west-2)]
+    (test-block-store
+      "s3-store" store
+      :blocks 25
+      :max-size 4096)
+    (blocks.store.s3/erase! store)))
