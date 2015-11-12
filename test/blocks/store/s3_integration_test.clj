84e46f2be5258965e5d9c4c5bd493401727d29e4
Greg Look <greg@greg-look.net>
2015-11-11 23:57:07 -0800

    Split up unit and integration tests.

---
+++ test/blocks/store/s3_integration_test.clj
@@ -0,0 +1,21 @@
+(ns blocks.store.s3-integration-test
+  (:require
+    (blocks.store
+      [s3 :refer [s3-store]]
+      [tests :refer [test-block-store]])
+    [clojure.test :refer :all]))
+
+
+(deftest ^:integration test-s3-store
+  (if (System/getenv "AWS_ACCESS_KEY_ID")
+    (let [store (s3-store "greglook-data"
+                          :prefix (str "s3-block-store."
+                                       (System/currentTimeMillis))
+                          :region :us-west-2)]
+      (test-block-store
+        "s3-store" store
+        :blocks 25
+        :max-size 4096
+        :clean false)
+      (blocks.store.s3/erase! store))
+    (println "No AWS_ACCESS_KEY_ID in environment, skipping integration tests.")))
--- test/blocks/store/s3_test.clj
+++ test/blocks/store/s3_test.clj
@@ -1,21 +1,24 @@
 (ns blocks.store.s3-test
   (:require
     (blocks.store
-      [s3 :refer [s3-store]]
-      [tests :refer [test-block-store]])
+      [s3 :refer [s3-store]])
     [clojure.test :refer :all]))
 
 
-; TODO: unit tests
-
-
-(deftest ^:integration test-s3-store
-  (let [store (s3-store "greglook-data"
-                        :prefix (str "s3-block-store."
-                                     (System/currentTimeMillis))
-                        :region :us-west-2)]
-    (test-block-store
-      "s3-store" store
-      :blocks 25
-      :max-size 4096)
-    (blocks.store.s3/erase! store)))
+(deftest store-construction
+  (is (thrown? IllegalArgumentException
+               (s3-store nil))
+      "bucket name should be required")
+  (is (thrown? IllegalArgumentException
+               (s3-store "   "))
+      "bucket name cannot be empty")
+  (is (some? (s3-store "foo-bar-data")))
+  (is (some? (s3-store "foo-bar-data" :credentials {:access-key "foo"
+                                                    :secret-key "bar"})))
+  (is (some? (s3-store "foo-bucket" :region :us-west-2)))
+  (is (thrown? IllegalArgumentException
+               (s3-store "foo-bucket" :region :unicorns-and-rainbows)))
+  (is (nil? (:prefix (s3-store "foo-bucket" :prefix ""))))
+  (is (nil? (:prefix (s3-store "foo-bucket" :prefix "/"))))
+  (is (= "foo/" (:prefix (s3-store "foo-bucket" :prefix "foo"))))
+  (is (= "bar/" (:prefix (s3-store "foo-bucket" :prefix "bar/")))))
