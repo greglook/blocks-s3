845bf0b7380626d1854da8834b8680c1ff50e64c
Greg Look <greg@greg-look.net>
2016-07-24 19:54:21 -0700

    Upgrade some dependencies.

--- project.clj
+++ project.clj
@@ -5,20 +5,24 @@
             :url "http://unlicense.org/"}
 
   :deploy-branches ["master"]
+  :pedantic? :abort
 
   :dependencies
-  [[com.amazonaws/aws-java-sdk-s3 "1.11.5"]
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojure/tools.logging "0.3.1"]
+   [com.amazonaws/aws-java-sdk-s3 "1.11.20"]
+   [commons-logging "1.2"]
    [mvxcvi/blocks "0.7.0"]
-   [mvxcvi/multihash "2.0.0"]
-   [org.clojure/clojure "1.7.0"]
-   [org.clojure/tools.logging "0.3.1"]]
+   [mvxcvi/multihash "2.0.0"]]
 
-  :aliases {"docs" ["do" ["codox"] ["doc-lit"]]
-            "doc-lit" ["marg" "--dir" "doc/marginalia"]
-            "coverage" ["with-profile" "+test,+coverage" "cloverage"]}
+  :aliases
+  {"docs" ["do" ["codox"] ["doc-lit"]]
+   "doc-lit" ["marg" "--dir" "doc/marginalia"]
+   "coverage" ["with-profile" "+test,+coverage" "cloverage"]}
 
-  :test-selectors {:unit (complement :integration)
-                   :integration :integration}
+  :test-selectors
+  {:unit (complement :integration)
+   :integration :integration}
 
   :codox
   {:metadata {:doc/format :markdown}
@@ -31,11 +35,15 @@
                'blocks.data.Block {'blocks.data.Block (partial into {})}}}
 
   :profiles
-  {:repl {:source-paths ["dev"]
-          :dependencies [[org.clojure/tools.namespace "0.2.11"]]}
-   :test {:dependencies [[commons-logging "1.2"]]
-          :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
-   :coverage {:plugins [[lein-cloverage "1.0.6"]]
-              :dependencies [[com.fasterxml.jackson.core/jackson-databind "2.7.4"]]
-              :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-                         "-Dorg.apache.commons.logging.simplelog.defaultlog=trace"]}})
+  {:repl
+   {:source-paths ["dev"]
+    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}
+
+   :test
+   {:jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
+
+   :coverage
+   {:plugins [[rfkm/lein-cloverage "1.0.8"]]
+    :dependencies [[com.fasterxml.jackson.core/jackson-databind "2.8.1"]]
+    :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
+               "-Dorg.apache.commons.logging.simplelog.defaultlog=trace"]}})
