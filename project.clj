b8729cc65a8a599a33ab70e70344a027718b0fec
Greg Look <greg@amperity.com>
2019-02-05 11:59:04 -0800

    Update project definition.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/blocks-s3 "0.4.1-SNAPSHOT"
+(defproject mvxcvi/blocks-s3 "2.0.0-SNAPSHOT"
   :description "Content-addressable S3 block store."
   :url "https://github.com/greglook/blocks-s3"
   :license {:name "Public Domain"
@@ -11,12 +11,10 @@
   :pedantic? :abort
 
   :dependencies
-  [[org.clojure/clojure "1.9.0"]
-   [org.clojure/tools.logging "0.4.0"]
-   [mvxcvi/blocks "1.1.0"]
-   [mvxcvi/multihash "2.0.3"]
-   [com.amazonaws/aws-java-sdk-s3 "1.11.271"]
-   [commons-logging "1.2"]]
+  [[org.clojure/clojure "1.10.0"]
+   [org.clojure/tools.logging "0.4.1"]
+   [mvxcvi/blocks "2.0.0-SNAPSHOT"]
+   [com.amazonaws/aws-java-sdk-s3 "1.11.491"]]
 
   :test-selectors
   {:default (complement :integration)
@@ -28,13 +26,13 @@
    :output-path "target/doc/api"}
 
   :whidbey
-  {:tag-types {'multihash.core.Multihash {'data/hash 'multihash.core/base58}
-               'blocks.data.Block {'blocks.data.Block (partial into {})}}}
+  {:tag-types {'multiformats.hash.Multihash {'multi/hash 'multiformats.hash/hex}
+               'blocks.data.Block {'blocks.data.Block (juxt :id :size :stored-at)}}}
 
   :profiles
   {:dev
-   {:dependencies
-    [[mvxcvi/test.carly "0.4.1"]]}
+   {:dependencies [[mvxcvi/blocks-test "2.0.0-SNAPSHOT"]
+                   [commons-logging "1.2"]]}
 
    :repl
    {:source-paths ["dev"]
@@ -44,7 +42,7 @@
    {:jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
 
    :coverage
-   {:plugins [[lein-cloverage "1.0.9"]]
-    :dependencies [[riddley "0.1.15"]]
+   {:plugins [[lein-cloverage "1.0.13"]]
+    ;:dependencies [[riddley "0.1.15"]]
     :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
                "-Dorg.apache.commons.logging.simplelog.defaultlog=trace"]}})
