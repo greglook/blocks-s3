20413c59ae08ab684092f2bf4545bcd1a4b3a627
Greg Look <greg@amperity.com>
2019-03-11 16:34:52 -0700

    Bump snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/blocks-s3 "2.0.0-SNAPSHOT"
+(defproject mvxcvi/blocks-s3 "2.0.1-SNAPSHOT"
   :description "Content-addressable S3 block store."
   :url "https://github.com/greglook/blocks-s3"
   :license {:name "Public Domain"
