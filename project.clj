f2406b166312ae2e5e09984d9b8de621db614ae6
Greg Look <greg@mvxcvi.com>
2020-04-20 23:42:12 -0700

    Upgrade dependency versions.

--- project.clj
+++ project.clj
@@ -11,11 +11,11 @@
   :pedantic? :abort
 
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [org.clojure/tools.logging "0.4.1"]
-   [mvxcvi/blocks "2.0.1"]
-   ; TODO: upgrade to software.amazon.awssdk/s3 v2
-   [com.amazonaws/aws-java-sdk-s3 "1.11.491"]]
+  [[org.clojure/clojure "1.10.1"]
+   [org.clojure/tools.logging "1.0.0"]
+   [mvxcvi/blocks "2.0.4"]
+   ;; TODO: upgrade to software.amazon.awssdk/s3 v2
+   [com.amazonaws/aws-java-sdk-s3 "1.11.765"]]
 
   :test-selectors
   {:default (complement :integration)
@@ -31,13 +31,13 @@
   :profiles
   {:dev
    {:dependencies
-    [[mvxcvi/blocks-tests "2.0.1"]
+    [[mvxcvi/blocks-tests "2.0.4"]
      [commons-logging "1.2"]]}
 
    :repl
    {:source-paths ["dev"]
     :dependencies
-    [[org.clojure/tools.namespace "0.2.11"]]}
+    [[org.clojure/tools.namespace "1.0.0"]]}
 
    :test
    {:jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
